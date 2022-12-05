b12eecc9963a12a9e25ca00eab37ebc37ac91259
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-12-06 02:45:35 +0300

    Modified TicketSearch.java file

--- src/main/java/my/idea/list/TicketSearch.java
+++ src/main/java/my/idea/list/TicketSearch.java
@@ -2,4 +2,65 @@ package my.idea.list;
 
 public class TicketSearch implements Comparable<TicketSearch> {
 
+    int id;
+    int price;
+    String departure;
+    String arrival;
+    int time;
+
+    public TicketSearch() {
+    }
+
+    public TicketSearch(int id, int price, String departure, String arrival, int time) {
+        this.id = id;
+        this.price = price;
+        this.departure = departure;
+        this.arrival = arrival;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    @Override
+    public int compareTo(TicketSearch o) {
+        return this.price - o.price;
+    }
 }
