adeef55f9bd1db26403c897374b5f8b0e476d46a
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-12-06 02:49:38 +0300

    Modified TicketSearchManager.java file

--- src/main/java/my/idea/list/TicketSearchManager.java
+++ src/main/java/my/idea/list/TicketSearchManager.java
@@ -1,5 +1,30 @@
 package my.idea.list;
 
+import java.util.Arrays;
+
 public class TicketSearchManager {
 
+    private TicketSearchRepository repository;
+
+    public TicketSearchManager(TicketSearchRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addTicket(TicketSearch item) {
+        repository.addTicket(item);
+    }
+
+    public TicketSearch[] findAllTickets(String from, String to) {
+        TicketSearch[] result = new TicketSearch[0];
+        for (TicketSearch item : repository.findAllTickets()) {
+            if (item.getDeparture().equals(from) && item.getArrival().equals(to)) {
+                TicketSearch[] temp = new TicketSearch[result.length + 1];
+                System.arraycopy(result, 0, temp, 0, result.length);
+                temp[temp.length - 1] = item;
+                result = temp;
+            }
+        }
+        Arrays.sort(result);
+        return result;
+    }
 }
